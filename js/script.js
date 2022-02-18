c5261ea1a2f32f77da6175d0f443964da727252c
DashkevihAS <99752659+DashkevihAS@users.noreply.github.com>
2022-02-18 16:40:07 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const sliderThumbs = new Swiper('.slider-thumbs', {
+    loop: true,
+    loopedSlides:4,
+    spaceBetween: 20,
+    slidesPerView: 3,
+    centeredSlides: true,
+});
+
+sliderThumbs.on('click', (swiper) => {
+    swiper.slideTo(swiper.clickedIndex)
+})
+
+const sliderMain = new Swiper('.slider-main', {
+    loop: true,
+    loopedSlides:4,
+});
+
+sliderThumbs.controller.control = sliderMain;
+sliderMain.controller.control = sliderThumbs;
